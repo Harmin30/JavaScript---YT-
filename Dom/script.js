4ab816646fb6c8fee543d6d307000b91b3344728
Harmin30 <160121177+Harmin30@users.noreply.github.com>
2025-07-10 13:04:12 +0530

    Add files via upload

---
+++ Dom/script.js
@@ -0,0 +1,9 @@
+// let headings = document.getElementsByClassName("heading-class");
+// console.dir(headings);
+// console.log(headings);
+
+let firstEl = document.querySelector(".heading"); //Gets the first element 
+console.dir(firstEl);
+
+let allEl = document.querySelectorAll(".heading"); //Gets all the elements
+console.dir(allEl)
